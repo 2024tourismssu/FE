a59db94ab5d841b35c8c8c1ccf7bcd4b98f1d3ed
Choi Yong Won <118504207+cywin1018@users.noreply.github.com>
2024-08-10 22:58:57 +0900

    chore : 메인 페이지의 카드 컴포넌트 생성

---
+++ src/components/Card.tsx
@@ -0,0 +1,31 @@
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import CardMedia from '@mui/material/CardMedia'
+import Typography from '@mui/material/Typography'
+import { CardActionArea } from '@mui/material'
+
+interface ActionAreaCardProps {
+   title: string
+   content?: string
+   image?: string
+   altText?: string
+}
+
+const ActionAreaCard = ({ title, content, image, altText }: ActionAreaCardProps) => {
+   return (
+      <Card sx={{ maxWidth: 345 }}>
+         <CardActionArea>
+            <CardMedia component="img" height="140" image={image} alt={altText} />
+            <CardContent>
+               <Typography gutterBottom variant="h5" component="div">
+                  {title}
+               </Typography>
+               <Typography variant="body2" color="text.secondary">
+                  {content}
+               </Typography>
+            </CardContent>
+         </CardActionArea>
+      </Card>
+   )
+}
+export default ActionAreaCard
