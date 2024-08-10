159be3ee68824fef9ce4eea9f0a31f1d54b55767
Choi Yong Won <118504207+cywin1018@users.noreply.github.com>
2024-08-10 21:50:48 +0900

    chore : Router 설정

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import Box from '@mui/material/Box'
+import Header from '../components/Header.tsx'
+
+const HomePage = () => {
+   return (
+      <Box>
+         <Header />
+         hello!
+      </Box>
+   )
+}
+
+export default HomePage
