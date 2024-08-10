6e7925e9fee69e3267d3347a89d950d093411019
Choi Yong Won <118504207+cywin1018@users.noreply.github.com>
2024-08-10 22:40:59 +0900

    chore : Router 설정 + HomePage 생성

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,5 +1,5 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import HomePage from '../pages/HomePage.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import HomePage from '../pages/home/HomePage.tsx'
 
 export const AppRouter = () => {
    return (
