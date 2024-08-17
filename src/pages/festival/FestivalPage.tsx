f4d76ef393dbfe28ca4adb055cb9e97f38e4e338
Choi Yong Won <118504207+cywin1018@users.noreply.github.com>
2024-08-17 20:21:33 +0900

    chore : react-calendar 수정 및 모바일 용 버튼 추가

--- src/pages/festival/FestivalPage.tsx
+++ src/pages/festival/FestivalPage.tsx
@@ -1,14 +1,42 @@
+import { useEffect, useState } from 'react'
 import Header from '../../components/Header.tsx'
 import Box from '@mui/material/Box'
 import CustomCalendar from '../../components/calendar/Calendar.tsx'
+import CalendarButton from '../../components/iconButton/CalendarButton.tsx'
 import styles from './styles/FestivalPage.module.scss'
 
 const FestivalPage = () => {
+   const [isMobile, setIsMobile] = useState<boolean>(window.innerWidth <= 768)
+   const [isCalendarVisible, setIsCalendarVisible] = useState<boolean>(false) // CustomCalendar의 가시성 상태
+
+   const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768)
+   }
+
+   const toggleCalendarVisibility = () => {
+      setIsCalendarVisible((prev) => !prev) // 가시성 상태를 토글
+   }
+
+   useEffect(() => {
+      window.addEventListener('resize', handleResize)
+      return () => {
+         window.removeEventListener('resize', handleResize)
+      }
+   }, [])
+
    return (
       <Box>
          <Header />
          <Box className={styles.container}>
-            <CustomCalendar />
+            {isMobile && (
+               <>
+                  {isCalendarVisible && <CustomCalendar />}
+                  <Box className={styles.calendarWrapper}>
+                     <CalendarButton onClick={toggleCalendarVisibility} />
+                  </Box>
+               </>
+            )}
+            {!isMobile && <CustomCalendar />}
          </Box>
       </Box>
    )
