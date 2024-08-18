5776f41c83c6c9076e9a9a9a8a74727e90a940fb
Choi Yong Won <118504207+cywin1018@users.noreply.github.com>
2024-08-18 16:05:07 +0900

    Feature/calendar (#3)
    * chore : react-calendar 추가 + 컴포넌트 기초셋팅
    
    * chore : react-calendar 코드 수정
    
    * chore : react-calendar 코드 수정
    
    * chore : react-calendar custom 수정
    
    * chore : react-calendar custom 수정
    
    * chore : react-calendar 수정 및 모바일 용 버튼 추가
    
    * chore : home 디자인한 것 복구
    
    * chore : calendar 날짜 2개 고르는것까지 완료

---
+++ src/components/calendar/Calendar.tsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import Box from '@mui/material/Box'
+import Calendar from 'react-calendar'
+import moment from 'moment'
+import './styles.css'
+
+type ValuePiece = Date | null
+type Value = ValuePiece | [ValuePiece, ValuePiece]
+
+interface CalendarTileProperties {
+   date: Date
+   view: 'month' | 'year' | 'decade' | 'century'
+}
+
+const CustomCalendar = () => {
+   const today = new Date()
+   const [value, setValue] = useState<Value>([null, null])
+
+   today.setHours(0, 0, 0, 0)
+
+   const disablePastDates = ({ date, view }: CalendarTileProperties): boolean => {
+      return view === 'month' && date < today
+   }
+
+   return (
+      <Box>
+         <Calendar
+            selectRange={true}
+            showNeighboringMonth={false}
+            calendarType="gregory"
+            formatDay={(_locale, date) => moment(date).format('D')}
+            formatMonthYear={(_locale, date) => moment(date).format('YYYY. MM')}
+            next2Label={null}
+            prev2Label={null}
+            tileDisabled={disablePastDates}
+            value={value}
+            onChange={setValue}
+         />
+      </Box>
+   )
+}
+
+export default CustomCalendar
