74559d3e62337bcaa33557832b3e9357ed2ffe2e
Choi Yong Won <118504207+cywin1018@users.noreply.github.com>
2024-08-25 22:25:42 +0900

    chore : 축제 컴포넌트 api에서 축제만 가져오도록 수정완료

--- src/components/festival/Festival.tsx
+++ src/components/festival/Festival.tsx
@@ -24,10 +24,13 @@ const Festival = () => {
             },
             responseType: 'json',
          })
-
-         console.log(response.data) // 응답 데이터 로그 출력
+         if (response.status == 200) {
+            const items: Array<{ cat3: string; title: string }> = response.data.response.body.items.item
+            // 아래의 item에서 사진 날짜 등등 뽑아와서 카드형태로 만들어서, 리스트로 보내면될듯!
+            items.filter((item) => item.cat3 === 'A02070100' || 'A02070200').forEach((item) => console.log(item))
+         }
       } catch (error) {
-         console.error('Error fetching festival data:', error) // 에러 핸들링
+         console.error('Error fetching festival data:', error)
       }
    }
 
