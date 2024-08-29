e60dec8e283d72024e24246598a4f3fec1dee7b9
Choi Yong Won <118504207+cywin1018@users.noreply.github.com>
2024-08-29 12:18:11 +0900

    Header 디자인변경 (#4)
    * chore : mui 및 sass 추가
    
    * chore : Header mui 이용해서 추가
    
    * chore : yarn update
    
    * chore : 헤더 파일 수정
    
    * chore : Router 설정
    
    * chore : Router 설정
    
    * chore : Router 설정 + HomePage 생성
    
    * Feature/home fe (#2)
    
    * chore : 메인 페이지의 카드 컴포넌트 생성
    
    * chore : 다양한 페이지 생성
    
    * chore : popup 페이지 생성
    
    * chore : 다른 페이지 라우팅
    
    * chore : 팝업스토어 라우팅 추가
    
    * chore : 로고 누르면 홈화면으로 가도록 라우팅 완료
    
    * Feature/calendar (#3)
    
    * chore : react-calendar 추가 + 컴포넌트 기초셋팅
    
    * chore : react-calendar 코드 수정
    
    * chore : react-calendar 코드 수정
    
    * chore : react-calendar custom 수정
    
    * chore : react-calendar custom 수정
    
    * chore : react-calendar 수정 및 모바일 용 버튼 추가
    
    * chore : home 디자인한 것 복구
    
    * chore : calendar 날짜 2개 고르는것까지 완료
    
    * chore : yarn update
    
    * chore : 헤더 파일 수정
    
    * chore : Router 설정
    
    * chore : Router 설정
    
    * chore : Router 설정 + HomePage 생성
    
    * chore : Header 코드 수정, 색깔 추가, 공연/행사로 수정
    
    * chore : 글자 semi-bold로 수정

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,172 @@
+import * as React from 'react'
+import { useState } from 'react'
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import Container from '@mui/material/Container'
+import Button from '@mui/material/Button'
+import SearchIcon from '@mui/icons-material/Search'
+import { styled } from '@mui/material/styles'
+import InputBase from '@mui/material/InputBase'
+import ImageComponent from '../Image.tsx'
+import Logo from '../../assets/images/Logo_v2.png'
+import IconButton from '@mui/material/IconButton'
+import MenuIcon from '@mui/icons-material/Menu'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+import { useNavigate } from 'react-router-dom'
+
+const pages = [
+   { label: '축제', path: '/festival' },
+   { label: '공연/행사', path: '/concert' },
+   { label: '문화시설', path: '/facility' },
+   { label: '팝업 스토어', path: '/popup' },
+]
+
+const Search = styled('div')(({ theme }) => ({
+   position: 'relative',
+   borderRadius: theme.shape.borderRadius,
+   backgroundColor: 'rgba(98, 180, 134, 0.23)',
+   marginLeft: '1rem',
+   marginRight: '1rem',
+   width: '40%',
+   [theme.breakpoints.up('md')]: {
+      width: '25%',
+   },
+}))
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+   padding: theme.spacing(0, 2),
+   height: '100%',
+   position: 'absolute',
+   pointerEvents: 'none',
+   display: 'flex',
+   alignItems: 'center',
+   justifyContent: 'center',
+}))
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+   color: 'inherit',
+   width: '100%',
+   '& .MuiInputBase-input': {
+      padding: theme.spacing(1, 1, 1, 0),
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create('width'),
+      [theme.breakpoints.up('md')]: {
+         width: '12ch',
+         '&:focus': {
+            width: '20ch',
+         },
+      },
+   },
+}))
+
+function Header() {
+   const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null)
+   const navigate = useNavigate()
+
+   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+      setAnchorElNav(event.currentTarget)
+   }
+
+   const handleCloseNavMenu = () => {
+      setAnchorElNav(null)
+   }
+
+   const handleNavigation = (path: string) => {
+      navigate(path)
+      handleCloseNavMenu() // 모바일 메뉴를 닫기 위해
+   }
+
+   return (
+      <AppBar
+         sx={{
+            backgroundColor: 'white',
+            color: 'black',
+            border: '1px solid black',
+            height: '10%',
+            display: 'flex',
+            justifyContent: 'center',
+         }}
+      >
+         <Container maxWidth={false} sx={{ width: '70%' }}>
+            <Toolbar disableGutters>
+               <ImageComponent src={Logo} sx={{ width: '100px', height: '50px', cursor: 'pointer' }} onClick={() => handleNavigation('/')} />
+               <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex', marginLeft: '1rem' } }}>
+                  {pages.map((page, index) => (
+                     <React.Fragment key={page.label}>
+                        <Button
+                           onClick={() => handleNavigation(page.path)}
+                           sx={{
+                              my: 2,
+                              color: 'black',
+                              fontWeight: 600,
+                              display: 'block',
+                              margin: 1.3,
+                              padding: 1,
+                              width: page.label === '팝업 스토어' ? '120px' : '90px',
+                              height: '45px',
+                              backgroundColor: 'rgba(98, 180, 134, 0.23)',
+                           }}
+                        >
+                           {page.label}
+                        </Button>
+                        {index === 2 && (
+                           <Box
+                              sx={{
+                                 width: '2px',
+                                 height: '35px',
+                                 backgroundColor: '#1E781E',
+                                 margin: '0 8px',
+                                 alignSelf: 'center',
+                              }}
+                           />
+                        )}
+                     </React.Fragment>
+                  ))}
+               </Box>
+
+               <Search>
+                  <SearchIconWrapper>
+                     <SearchIcon />
+                  </SearchIconWrapper>
+                  <StyledInputBase placeholder="Search…" inputProps={{ 'aria-label': 'search' }} />
+               </Search>
+
+               <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                  <IconButton size="large" aria-label="account of current user" aria-controls="menu-appbar" aria-haspopup="true" onClick={handleOpenNavMenu}>
+                     <MenuIcon />
+                  </IconButton>
+                  <Menu
+                     id="menu-appbar"
+                     anchorEl={anchorElNav}
+                     anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'right',
+                     }}
+                     keepMounted
+                     transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                     }}
+                     open={Boolean(anchorElNav)}
+                     onClose={handleCloseNavMenu}
+                     sx={{
+                        display: { xs: 'block', md: 'none' },
+                     }}
+                  >
+                     {pages.map((page) => (
+                        <MenuItem key={page.label} onClick={() => handleNavigation(page.path)}>
+                           <Typography textAlign="center">{page.label}</Typography>
+                        </MenuItem>
+                     ))}
+                  </Menu>
+               </Box>
+            </Toolbar>
+         </Container>
+      </AppBar>
+   )
+}
+
+export default Header
