9804c9e7466e28e42a56f0bc7b18c7ba06ffb609
Choi Yong Won <118504207+cywin1018@users.noreply.github.com>
2024-09-15 22:59:14 +0900

    chore : 문화시설 탭 제작

---
+++ src/components/facility/Facility.tsx
@@ -0,0 +1,52 @@
+import axios from 'axios'
+
+const Facility = async () => {
+   const apiKey = import.meta.env.VITE_API_KEY
+   const url = 'http://apis.data.go.kr/B551011/KorService1/areaBasedSyncList1'
+
+   try {
+      const response = await axios.get(url, {
+         params: {
+            numOfRows: 20,
+            pageNo: 1,
+            MobileOS: 'ETC',
+            mobileApp: 'AppTest',
+            _type: 'json',
+            listYN: 'Y',
+            arrange: 'A',
+            contentTypeId: 14,
+            areaCode: 6,
+            sigunguCode: 4,
+            serviceKey: apiKey,
+         },
+         headers: {
+            Accept: 'application/json',
+         },
+         responseType: 'json',
+      })
+
+      if (response.status === 200) {
+         const items = response.data.response.body.items.item
+         console.log(items)
+         return items
+            .filter((item: any) => {
+               const cat2Number = item.cat2
+               return cat2Number === 'A0206'
+            })
+            .map((item: any) => ({
+               title: item.title,
+               place: item.addr1 + (item.addr2 || ''),
+               startDate: item.eventstartdate,
+               endDate: item.eventenddate,
+               image: item.firstimage,
+               contentId: item.contentid,
+               contentTypeId: item.contenttypeid,
+            }))
+      }
+   } catch (error) {
+      console.error('Error fetching festival data:', error)
+      return []
+   }
+}
+
+export default Facility
